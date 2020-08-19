eacb5378db6a634dc537df6bec5011029c2af8ef
Stefano Mezzini <stefano@menouno.com>
2020-08-19 11:59:10 -0600

    last_plot => ggplot::last_plot

--- functions/save.plt.R
+++ functions/save.plt.R
@@ -1,4 +1,5 @@
-save.plt <- function(plt = last_plot(), dir, width = 8, height = 6, dpi = 300) {
+save.plt <- function(plt = ggplot2::last_plot(), dir, width = 8, height = 6,
+                     dpi = 300) {
   ggsave(filename = paste0('plots/', dir), plot = plt,
          width = width, height = height, units = 'in', dpi = 300)
 }
