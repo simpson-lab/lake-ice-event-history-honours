6734759aa4fb94906187f455122b29a65f76f2d8
Stefano Mezzini <stefano@menouno.com>
2021-09-06 10:54:54 -0600

    added `scale` argument

--- functions/save.plt.R
+++ functions/save.plt.R
@@ -1,5 +1,5 @@
 save.plt <- function(plt = ggplot2::last_plot(), dir, width = 8, height = 6,
-                     dpi = 300) {
+                     dpi = 300, scale = 1) {
   ggsave(filename = paste0('plots/', dir), plot = plt,
-         width = width, height = height, units = 'in', dpi = 300)
+         width = width, height = height, units = 'in', dpi = 300, scale = scale)
 }
