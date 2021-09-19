7190adad9c83876163b4783b035c5f727621a1f2
Stefano Mezzini <stefano@menouno.com>
2021-09-19 08:35:23 -0600

    specified white background

--- functions/save.plt.R
+++ functions/save.plt.R
@@ -1,5 +1,6 @@
 save.plt <- function(plt = ggplot2::last_plot(), dir, width = 8, height = 6,
                      dpi = 300, scale = 1) {
-  ggsave(filename = paste0('plots/', dir), plot = plt,
-         width = width, height = height, units = 'in', dpi = 300, scale = scale)
+  ggplot2::ggsave(filename = paste0('plots/', dir), plot = plt, width = width,
+                  height = height, units = 'in', dpi = 300, scale = scale,
+                  bg = 'white')
 }
